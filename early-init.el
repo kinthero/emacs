42c7b2e11c335617a45ea3c0b634a63a86e85353
kinther <jl.liang@foxmail.com>
2026-08-16 22:56:01 +0800

    Initial Emacs configuration

---
+++ early-init.el
@@ -0,0 +1,21 @@
+;; -*- lexical-binding: t; -*-
+
+;; Frame
+(setq default-frame-alist
+      '((left . 0.5)
+	(top . 0.5)
+	(width . 0.6)
+	(height . 0.6)
+	(internal-border-width . 8)
+	(menu-bar-lines . 0)
+	(tool-bar-lines . 0)
+	(vertical-scroll-bars . nil)
+	(horizontal-scroll-bars . nil)
+	;; (font . "FiraCode Nerd Font Mono-14")
+	(font . "LXGW WenKai Mono GB-14")
+	;; (alpha . (80 . 100))
+	(left-fringe . 0)
+	(right-fringe . 0)))
+
+(setq inhibit-startup-screen t
+      initial-scratch-message nil)
---
+++ init.el
@@ -0,0 +1,3 @@
+;; -*- lexical-binding: t; -*-
+
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))

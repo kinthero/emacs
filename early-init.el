5aaf21e83d40e1e9a75860cbcac658759e084d9d
kinther <jl.liang@foxmail.com>
2026-08-24 00:07:06 +0800

    update

--- early-init.el
+++ early-init.el
@@ -3,44 +3,43 @@
 ;; Frame
 (setq default-frame-alist
       '((left . 0.5)
-	(top . 0.5)
-	(width . 0.6)
-	(height . 0.6)
-	;; (font . "FiraCode Nerd Font Mono-14")
-	(font . "LXGW WenKai Mono GB-14")
-	;; (alpha . (80 . 100))
+        (top . 0.5)
+        (width . 0.6)
+        (height . 0.6)
+        ;; (font . "FiraCode Nerd Font Mono-14")
+        (font . "LXGW WenKai Mono GB-14")
+        ;; (alpha . (80 . 100))
         (undecorated . t)
-	(internal-border-width . 8)
-	;; fix undecorated drag size problem
+        (internal-border-width . 8)
+        ;; fix undecorated drag size problem
         (drag-internal-border . 1) 
         (drag-with-tab-line . t)
-	(left-fringe . 0)
-	(right-fringe . 0)
-	(cursor-color . "brown")
-	(menu-bar-lines . 0)
-	(tool-bar-lines . 0)
-	(vertical-scroll-bars . nil)
-	(horizontal-scroll-bars . nil)))
+        (left-fringe . 0)
+        (right-fringe . 0)
+        (cursor-color . "brown")
+        (menu-bar-lines . 0)
+        (tool-bar-lines . 0)
+        (vertical-scroll-bars . nil)
+        (horizontal-scroll-bars . nil)))
 
 ;; modeline
-
 (set-face-attribute 'mode-line nil
                     :inherit 'default
                     :background 'unspecified
-		    :box nil
-		    :overline "#eef4f9")
+                    :box nil
+                    :overline "#eef4f9")
 
 (set-face-attribute 'mode-line-active nil
                     :inherit 'default
                     :background 'unspecified
                     :overline "orange"
-		    :box nil)
+                    :box nil)
 
 (set-face-attribute 'mode-line-inactive nil
                     :inherit 'default
                     :background 'unspecified
                     :overline "#7f67bd"
-		    :box nil)
+                    :box nil)
 
 ;; tab bar
 (setq tab-bar-show 1)
@@ -52,18 +51,18 @@
 (set-face-attribute 'tab-bar nil
                     :inherit 'default
                     :background 'unspecified
-  		    :box nil)
+                    :box nil)
 
 (set-face-attribute 'tab-bar-tab nil
                     :inherit 'default
                     :background 'unspecified
-  		    :box nil)
+                    :box nil)
 
 (set-face-attribute 'tab-bar-tab-inactive nil
                     :inherit 'default
                     :background 'unspecified
-		    :foreground "gray50"
-  		    :box nil)
+                    :foreground "gray50"
+                    :box nil)
 
 (setq inhibit-startup-screen t
       initial-scratch-message nil)

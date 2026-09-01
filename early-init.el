8a4b8e36691de098fd5f4b6bd22f8dd4ddcda2b4
kinther <jl.liang@foxmail.com>
2026-09-01 20:40:24 +0800

    update

--- early-init.el
+++ early-init.el
@@ -22,48 +22,6 @@
         (vertical-scroll-bars . nil)
         (horizontal-scroll-bars . nil)))
 
-;; modeline
-(set-face-attribute 'mode-line nil
-                    :inherit 'default
-                    :background 'unspecified
-                    :box nil
-                    :overline "#eef4f9")
-
-(set-face-attribute 'mode-line-active nil
-                    :inherit 'default
-                    :background 'unspecified
-                    :overline "orange"
-                    :box nil)
-
-(set-face-attribute 'mode-line-inactive nil
-                    :inherit 'default
-                    :background 'unspecified
-                    :overline "#7f67bd"
-                    :box nil)
-
-;; tab bar
-(setq tab-bar-show 1)
-(setq tab-bar-close-button-show nil)
-(setq tab-bar-tab-hints t)
-(setq tab-bar-new-tab-choice "*scratch*")
-(setq tab-bar-format '(tab-bar-format-tabs tab-bar-separator))
-
-(set-face-attribute 'tab-bar nil
-                    :inherit 'default
-                    :background 'unspecified
-                    :box nil)
-
-(set-face-attribute 'tab-bar-tab nil
-                    :inherit 'default
-                    :background 'unspecified
-                    :box nil)
-
-(set-face-attribute 'tab-bar-tab-inactive nil
-                    :inherit 'default
-                    :background 'unspecified
-                    :foreground "gray50"
-                    :box nil)
-
 (setq inhibit-startup-screen t
       inhibit-splash-screen t
       initial-buffer-choice nil
